bf82e0af8bf8058c48f56bef879da6be7fd9e77f
Krasnov Robert <krasnovrobert.9@gmail.com>
2021-07-15 14:21:49 +0300

    сделал фон, добавил клетки

--- Assets/C# scripts/CameraControl.cs
+++ Assets/C# scripts/CameraControl.cs
@@ -4,86 +4,62 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    [Range(0, 10f)]
-    public float MoveSpeed = 10f;
-    [Range(0f, 5f)]
-    public float Sensitivity = 3;
-    float tempSens;
-    public bool IsDragging { get; private set; }
-    public new Camera Camera { get; private set; }
-    private Vector2 tempCenter, targetDirection, tempMousePos;
-    public Transform transform;
-    Vector3 position = Vector3.zero;
+    public float dragSpeed = 2;
+    private Vector3 dragOrigin;
+
+    public bool cameraDragging = true;
+
+    public float outerLeft = -10f;
+    public float outerRight = 10f;
 
     private void Start()
     {
-        this.Camera = GetComponent<Camera>();
+    
     }
 
     // Update is called once per frame
     private void Update()
     {
-        UpdateInput();
-        UpdatePosition();
-    }
+        Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
 
-    private void UpdateInput() 
-    {
-        Vector2 mousePosition = Input.mousePosition;
-        if (Input.GetMouseButtonDown(0)) OnPointDown(mousePosition);
-        else if (Input.GetMouseButtonUp(0)) OnPointUp(mousePosition);
-        else if (Input.GetMouseButton(0)) OnPointMove(mousePosition);
-    }
-    private void UpdatePosition() 
-    {
-  
-        float speed = Time.deltaTime * this.MoveSpeed;
-        if (this.IsDragging) 
-            this.tempSens = this.Sensitivity;
-        else 
-            this.tempSens = Mathf.Lerp(this.tempSens, 0f, speed);
+        float left = Screen.width * 0.2f;
+        float right = Screen.width - (Screen.width * 0.2f);
 
-        var newPosition = this.targetDirection * this.tempSens;
-        this.transform.position = Vector2.Lerp(this.position, newPosition, speed);
-    }
-    private Vector2 GetWorldPoint(Vector2 mousePosition)
-    {
-        Vector2 point = Vector2.zero;
-        Ray ray = this.Camera.ScreenPointToRay(mousePosition);
+        float up = Screen.height * 0.2f;
+        float down = Screen.height - (Screen.height * 0.2f);
 
-        Vector3 normal = Vector3.forward;
-        Plane plane = new Plane(normal, position);
+        //if (mousePosition.x < left)
+        //{
+        //    cameraDragging = true;
+        //}
+        //else if (mousePosition.x > right)
+        //{
+        //    cameraDragging = true;
+        //}
 
-        float distance;
-        plane.Raycast(ray, out distance);
-        point = ray.GetPoint(distance);
-
-        return point;
-    }
-    private void OnPointDown(Vector2 mousePosition) 
-    {
-        this.tempCenter = GetWorldPoint(mousePosition);
-        this.targetDirection = Vector2.zero;
-        this.tempMousePos = mousePosition;
-        this.IsDragging = true;
-    }
-    private void OnPointMove(Vector2 mousePosition)
-    {
-        if (this.IsDragging)
+        if (mousePosition.y < up)
         {
-            Vector2 point = GetWorldPoint(mousePosition);
-
-            var sqrDist = (this.tempCenter - point).sqrMagnitude;
-            if (sqrDist > 0.1f)
-            {
-                this.targetDirection = (this.tempMousePos - mousePosition).normalized;
-                this.tempMousePos = mousePosition;
-            }
+            cameraDragging = true;
         }
-    }
+        else if (mousePosition.y > down)
+        {
+            cameraDragging = true;
+        }
 
-    private void OnPointUp(Vector2 mousePosition) 
-    {
-        this.IsDragging = false;
+        if (cameraDragging)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                dragOrigin = Input.mousePosition;
+                return;
+            }
+
+            if (!Input.GetMouseButton(0)) return;
+
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+            Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+
+            transform.Translate(move, Space.World);
+        }
     }
 }
